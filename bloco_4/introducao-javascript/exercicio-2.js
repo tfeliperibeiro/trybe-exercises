00df14fe8131302483595b4b4b0aeb7a724f8504
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-02-26 16:04:52 -0300

    Exericio 2 finalizado

---
+++ bloco_4/introducao-javascript/exercicio-2.js
@@ -0,0 +1,16 @@
+/*Faça um programa que retorne o maior de dois números. 
+Defina no começo do programa duas variáveis com os valores 
+que serão comparados.*/
+
+
+
+let n1 = 30;
+let n2 = 25;
+
+if (n1 > n2) {
+  console.log(n1 + " é maior.")
+} else {
+  console.log(n2 + " é maior.")
+}
+
+
