9e07650fefc4185539332d2be9ac93f3e911a04d
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-02-26 12:48:20 -0300

    Exercicio 1 finalizado

---
+++ bloco_4/introducao-javascript/exercicio-1.js
@@ -0,0 +1,19 @@
+
+/*Faça cinco programas, um para cada operação aritmética básica. 
+Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. 
+Faça programas para:*/
+
+/*Adição
+Subtração
+Multiplicação
+Divisão
+Módulo*/
+
+let a = 15;
+let b = 18;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
