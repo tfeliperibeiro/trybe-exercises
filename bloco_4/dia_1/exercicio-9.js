57c3b257eac6175c6d56ae83bde5f9a4e4f82571
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-02-27 17:58:16 -0300

    Exercise nine

---
+++ bloco_4/dia_1/exercicio-9.js
@@ -0,0 +1,26 @@
+/*Escreva um programa que defina três números em variáveis no seu começo e 
+retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+Bonus: use somente um if .*/
+
+let n1 = 2;
+let n2 = 3;
+let n3 = 4;
+
+if (n1 % 2 !== 0 || n2 % 2 !== 0 || n3 % 2 !== 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+/*------------------------------------------------------*/
+
+let numeros = [2, 4, 6];
+let resultado = false;
+
+for (let index = 0; index < numeros.length; index++) {
+  if (numeros[index] % 2 !== 0) {
+    resultado = true;
+  }
+}
+
+console.log(resultado);
