b69631c78980773f14f8cc6e53122fcfeea42adc
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-02-28 19:04:47 -0300

    Exercise ten

---
+++ bloco_4/dia_1/exercicio-10.js
@@ -0,0 +1,11 @@
+let custoProduto = 5;
+let valorVenda = 10;
+
+
+if (custoProduto >= 0 && valorVenda >= 0) {
+  let totalProduto = custoProduto * 1.2;
+  let lucroProduto = (valorVenda - totalProduto) * 1000;
+  console.log(lucroProduto);
+} else {
+  console.log('Erro, valor inválido.')
+}
--- bloco_4/dia_2/exercicio-2.js
+++ bloco_4/dia_2/exercicio-2.js
@@ -2,7 +2,6 @@
 //valores contidos no array e imprimir o resultado;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
 let soma = 0;
 
 for(let index = 0; index < numbers.length; index++){
