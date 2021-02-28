a908c6fa05ca760abdc6908366496bec456d867b
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-02-28 19:16:14 -0300

    Exercise ten

--- bloco_4/dia_1/exercicio-10.js
+++ bloco_4/dia_1/exercicio-10.js
@@ -1,9 +1,11 @@
 let custoProduto = 5;
 let valorVenda = 10;
 
-
+//verificando se existe numero negativo
 if (custoProduto >= 0 && valorVenda >= 0) {
+  //calculando produto com 20% de acrescimo
   let totalProduto = custoProduto * 1.2;
+  //calculando lucro do produto em cima de mil vendas
   let lucroProduto = (valorVenda - totalProduto) * 1000;
   console.log(lucroProduto);
 } else {
