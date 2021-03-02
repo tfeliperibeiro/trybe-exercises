941b98154096a99998885c9531749eaef48201e5
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-02 11:57:31 -0300

    Exercicio 2

--- bloco_4/dia_4/exercicio-1.js
+++ bloco_4/dia_4/exercicio-1.js
@@ -1,10 +1,11 @@
+//Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+
 let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
 
-//Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 info.personagem2 = 'Thiago';
 
 console.log('Bem vindo(a): ' + info.personagem);
